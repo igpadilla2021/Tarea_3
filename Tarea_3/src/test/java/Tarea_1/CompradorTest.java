bab2e65b266896125b982ef9ca3531612644abb1
igpadilla2021 <nachoclo2470@gmail.com>
2023-05-30 19:30:34 -0400

    "Creacion de Test asociado a expendedor"

--- Tarea_3/src/test/java/Tarea_1/CompradorTest.java
+++ Tarea_3/src/test/java/Tarea_1/CompradorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
-class MainTest {
+class CompradorTest {
     private Expendedor expendedor;
     private Comprador c;
 
